6ba950820846cc5bb8742db6e18552cd4b93edeb
Daniel Clarke <deliotclarke@gmail.com>
2020-06-22 15:54:31 -0500

    added yargs library and create yargs command line commands, console logged via yargs.argv

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,44 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const command = process.argv[2];
+// customize yargs version
+yargs.version('yourboy_1.1.0');
 
-if (command === 'add') {
-  console.log('adding note!');
-} else if (command === 'remove') {
-  console.log('byeeee note!');
-}
+// create add command
+yargs.command({
+  command: 'add',
+  describe: 'add a new note. doi.',
+  handler: function () {
+    console.log('adding a new note');
+  },
+});
+
+//create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'remove an existing note.',
+  handler: function () {
+    console.log('removing that one note.');
+  },
+});
+
+//create list command
+yargs.command({
+  command: 'list',
+  describe: 'list all notes.',
+  handler: function () {
+    console.log('here is a list of notes.');
+  },
+});
+
+//create read command
+yargs.command({
+  command: 'read',
+  describe: 'read your own note.',
+  handler: function () {
+    console.log('this is where your note would be if you had one.');
+  },
+});
+
+console.log(yargs.argv);
