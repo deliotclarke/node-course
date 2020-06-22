b8674560a181eb5d5e252be724f6d21f537f9a07
Daniel Clarke <deliotclarke@gmail.com>
2020-06-22 14:06:32 -0500

    added validator npm package and utilized two methods.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,8 @@
+const validator = require('validator');
 const getNotes = require('./notes');
 
 console.log(getNotes());
+
+console.log(validator.isEmail('ItmeClarke@example.com'));
+
+console.log(validator.isURL('www.mead.io'));
