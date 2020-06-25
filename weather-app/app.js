406f8a873b394251a95e46c78e86f2b5d06a7ec9
Daniel Clarke <deliotclarke@gmail.com>
2020-06-25 13:38:10 -0500

    added f query for farenheit forecast and created forecast variable to make life a little easier in string interpolation.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,13 @@
 const request = require('postman-request');
 const access = require('./weatherKey');
 
-const url = `http://api.weatherstack.com/current?access_key=${access.key}&query=37.8267,-122.4233`;
+const location = '37.8267,-122.4233';
 
-request({ url }, (error, response) => {
-  const parsedData = JSON.parse(response.body);
-  console.log(parsedData.current);
+const url = `http://api.weatherstack.com/current?access_key=${access.key}&query=${location}&units=f`;
+
+request({ url, json: true }, (error, response) => {
+  const forecast = response.body.current;
+  console.log(
+    `It is currently ${forecast.temperature} degrees out. It is ${forecast.weather_descriptions[0]} with ${forecast.precip}% chance of rain.`
+  );
 });
