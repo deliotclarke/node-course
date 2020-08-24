b24c765c01f13fd1bc7e7c799fa9c2ad7d8b1a7d
Daniel Clarke <deliotclarke@gmail.com>
2020-08-24 16:05:20 -0500

    client side fetch working

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,9 @@
-console.log('client side javascript file is loading');
+fetch('http://localhost:3000/weather?address=boston')
+  .then((res) => res.json())
+  .then((data) => {
+    if (data.error) {
+      console.log(data.message, data.error);
+    } else {
+      console.log(data.location, data.forecast);
+    }
+  });
