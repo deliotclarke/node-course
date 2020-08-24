9c19a05aaa1196fac489d9e5d3dd09219a749021
Daniel Clarke <deliotclarke@gmail.com>
2020-08-24 16:18:10 -0500

    created and hooked search bar up to client side fetch call.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,9 +1,16 @@
-fetch('http://localhost:3000/weather?address=boston')
-  .then((res) => res.json())
-  .then((data) => {
-    if (data.error) {
-      console.log(data.message, data.error);
-    } else {
-      console.log(data.location, data.forecast);
-    }
-  });
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const userInput = search.value;
+  fetch(`http://localhost:3000/weather?address=${userInput}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) {
+        console.log(data.message, data.error);
+      } else {
+        console.log(data.location, data.forecast);
+      }
+    });
+});
